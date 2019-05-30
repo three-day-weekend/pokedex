e5cc9b2e106796c6bbd506d41b86640de1939995
sepuckett86 <sepuckett86@gmail.com>
2019-05-30 14:01:52 -0700

    rendered app and header

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import Component from './Component.js';
+import Header from './Header.js';
+
+
+class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header();
+        const headerDOM = header.render();
+        
+        dom.prepend(headerDOM);
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/ `
+            <div>
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default App;
